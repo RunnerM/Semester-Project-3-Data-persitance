e25ba85bb66e123ec59b3c07e04f1dc590878ac4
Marton Pentek <pentek.marci@gmail.com>
2020-11-06 19:48:56 +0100

    initial

---
+++ src/main/java/via/sep3/group7/newsdataapi/NewsdataapiApplication.java
@@ -0,0 +1,13 @@
+package via.sep3.group7.newsdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsdataapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsdataapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/via/sep3/group7/newsdataapi/NewsdataapiApplicationTests.java
@@ -0,0 +1,13 @@
+package via.sep3.group7.newsdataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsdataapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
