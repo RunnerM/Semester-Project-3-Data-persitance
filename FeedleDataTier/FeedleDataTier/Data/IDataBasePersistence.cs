e20f8c22d88807740d4592e27b544484cbdbe245
Eugene4JD <36538202+Eugene4JD@users.noreply.github.com>
2020-12-02 19:13:48 +0100

    asdf

---
+++ FeedleDataTier/FeedleDataTier/Data/IDataBasePersistence.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using FeedleDataTier.Models;
+
+namespace FeedleDataTier.Data
+{
+    public interface IDataBasePersistence
+    {
+        User UpdateUser(string userAsJson);
+        Post UpdatePost(string postAsJson);
+        User AddUser(string userAsJson);
+        Post AddPost(string postAsJson);
+        User VerifyUser(string login, string password);
+        List<Post> GetPosts();
+        
+    }
+}
