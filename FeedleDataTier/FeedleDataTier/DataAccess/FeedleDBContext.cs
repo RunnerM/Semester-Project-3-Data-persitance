0bc45657d455957b2218e6b561e8b51e6b7a6511
Eugene4JD <36538202+Eugene4JD@users.noreply.github.com>
2020-12-02 18:43:57 +0100

    ew

---
+++ FeedleDataTier/FeedleDataTier/DataAccess/FeedleDBContext.cs
@@ -0,0 +1,32 @@
+﻿using FeedleDataTier.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FeedleDataTier.DataAccess
+{
+    public class FeedleDBContext : DbContext
+    {
+        private DbSet<User> Users;
+        private DbSet<Post> Posts;
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Database=Feedle_db;Username=postgres;Password=3228");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserConversation>()
+                .HasKey(uc => new {uc.UserId, uc.ConversationId});
+
+            modelBuilder.Entity<UserConversation>()
+                .HasOne(uc => uc.User)
+                .WithMany(user => user.UserConversations)
+                .HasForeignKey(uc => uc.UserId);
+
+            modelBuilder.Entity<UserConversation>()
+                .HasOne(uc => uc.Conversation)
+                .WithMany(conversation => conversation.UserConversations)
+                .HasForeignKey(uc => uc.ConversationId);
+        }
+    }
+}
--- src/main/java/via/sep3/group7/newsdataapi/model/SubscriptionReference.java
+++
@@ -1,8 +0,0 @@
-package via.sep3.group7.newsdataapi.model;
-
-public class SubscriptionReference {
-    //Todo: still have no idea about this class.
-
-    public SubscriptionReference() {
-    }
-}
