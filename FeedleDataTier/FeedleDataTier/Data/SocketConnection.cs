d075a4f46dd930d446e3fa16d35fd424c1c19675
Eugene4JD <36538202+Eugene4JD@users.noreply.github.com>
2020-12-09 12:08:28 +0100

    FIIIXXX

--- FeedleDataTier/FeedleDataTier/Data/SocketConnection.cs
+++ FeedleDataTier/FeedleDataTier/Data/SocketConnection.cs
@@ -8,57 +8,62 @@ using System.Net.Sockets;
  using System.Threading;
  using FeedleDataTier.Models;
  using FeedleDataTier.Network;
+ using Microsoft.VisualBasic.CompilerServices;
 
  namespace FeedleDataTier.Data
 {
     public class SocketConnection
     {
         public IDataBasePersistence DbPersistence { get; set; }
-        public IPAddress ip { get; set; }
-        public TcpListener Listener { get; set; }
+        
+        public IPHostEntry host { get; set; }
+
+        public IPAddress ipAddress { get; set; }  
+        public IPEndPoint serverAddress {get; set;}
+        public IPEndPoint localEndPoint { get; set; }
 
         public SocketConnection(IDataBasePersistence dbPersistence)
         {
-            this.ip = IPAddress.Parse("127.0.0.1");
-            Listener = new TcpListener(ip,5000);
+            host = Dns.GetHostEntry("127.0.0.1"); 
+            ipAddress = host.AddressList[0]; 
+            serverAddress = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+            localEndPoint = new IPEndPoint(ipAddress, 5000);
             this.DbPersistence = dbPersistence;
         }
-
-        public SocketConnection(string ipAddress, int portNumber, IDataBasePersistence dbPersistence)
-        {
-            this.ip = IPAddress.Parse(ipAddress);
-            Listener = new TcpListener(ip,portNumber);
-            this.DbPersistence = dbPersistence;
-        }
-
+        
         public void Start()
         {
-            Listener.Start();
+            Console.WriteLine("Server started..");
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream,ProtocolType.Tcp);
+            serverSocket.Bind(serverAddress);
+            serverSocket.Listen(10);
             Thread thread = new Thread(() =>
             {
-                Console.WriteLine("Server started..");
                 while (true)
                 {
-                    TcpClient client = Listener.AcceptTcpClient();
-                    Thread clientThread = new Thread(() =>ClientThread(client));
+                    Socket socketToClient = serverSocket.Accept();
+                    Console.WriteLine("Client connected...");
+                    Thread clientThread = new Thread(() =>ClientThread(socketToClient));
                     clientThread.Start();
                 }
             });
             thread.Start();
         }
-        private void ClientThread(TcpClient client)
+        private void ClientThread(Socket client)
         {
-            Console.WriteLine("Client connected,Thread started");
-            NetworkStream stream = client.GetStream();
+            Console.WriteLine("Client thread started");
             while (true)
             {
                 //read
-                byte[] dataFromClient = new byte[1024*1024*8];
-                int bytesRead = stream.Read(dataFromClient, 0, dataFromClient.Length);
-                string message = Encoding.ASCII.GetString(dataFromClient, 0, bytesRead);
+                byte[] rcvLenBytes = new byte[4];
+                client.Receive(rcvLenBytes);
+                int rcvLen = BitConverter.ToInt32(rcvLenBytes, 0);
+                byte[] dataFromClient = new byte[rcvLen];
+                client.Receive(dataFromClient);
+                string message = Encoding.ASCII.GetString(dataFromClient, 0, rcvLen);
                 Console.WriteLine(message);
                 Request request =
-                    JsonSerializer.Deserialize<Request>(Encoding.ASCII.GetString(dataFromClient, 0, bytesRead));
+                    JsonSerializer.Deserialize<Request>(Encoding.ASCII.GetString(dataFromClient, 0, rcvLen));
                 if (message.Equals("stop"))
                 {
                     Console.WriteLine(message);
@@ -69,27 +74,35 @@ using System.Net.Sockets;
                 {
                     case RequestType.AddPost:
                         
-                        AddPostRequest addPostRequest = JsonSerializer.Deserialize<AddPostRequest>(Encoding.ASCII.GetString(dataFromClient, 0,
-                            bytesRead));
+                        AddPostRequest addPostRequest = JsonSerializer.Deserialize<AddPostRequest>(message);
                         DbPersistence.AddPost(addPostRequest.Post);
-                        stream.Write(dataFromClient, 0, dataFromClient.Length);
+                        int toSendLenAddPost = Encoding.ASCII.GetByteCount(message);
+                        byte[] toSendBytesAddPost = Encoding.ASCII.GetBytes(message);
+                        byte[] toSendLenBytesAddPost = BitConverter.GetBytes(toSendLenAddPost);
+                        client.Send(toSendLenBytesAddPost);
+                        client.Send(toSendBytesAddPost);
                         break;
                     
                     case RequestType.DeletePost :
                         
-                        DeletePostRequest deletePostRequest = JsonSerializer.Deserialize<DeletePostRequest>(
-                            Encoding.ASCII.GetString(dataFromClient, 0,
-                                bytesRead));
+                        DeletePostRequest deletePostRequest = JsonSerializer.Deserialize<DeletePostRequest>(message);
                         DbPersistence.DeletePost(deletePostRequest.PostId);
-                        stream.Write(dataFromClient, 0, dataFromClient.Length);
+                        int toSendDelPost = Encoding.ASCII.GetByteCount(message);
+                        byte[] toSendBytesDelPost = Encoding.ASCII.GetBytes(message);
+                        byte[] toSendLenBytesDelPost = BitConverter.GetBytes(toSendDelPost);
+                        client.Send(toSendLenBytesDelPost);
+                        client.Send(toSendBytesDelPost);
                         break;
                     
                     case RequestType.DeleteUser :
 
-                        DeleteUserRequest deleteUserRequest = JsonSerializer.Deserialize<DeleteUserRequest>(
-                            Encoding.ASCII.GetString(dataFromClient, 0, bytesRead));
+                        DeleteUserRequest deleteUserRequest = JsonSerializer.Deserialize<DeleteUserRequest>(message);
                         DbPersistence.DeleteUser(deleteUserRequest.UserId);
-                        stream.Write(dataFromClient, 0, dataFromClient.Length);
+                        int toSendDelUser = Encoding.ASCII.GetByteCount(message);
+                        byte[] toSendBytesDelUser = Encoding.ASCII.GetBytes(message);
+                        byte[] toSendLenBytesDelUser = BitConverter.GetBytes(toSendDelUser);
+                        client.Send(toSendLenBytesDelUser);
+                        client.Send(toSendBytesDelUser);
                         break;
                     
                     case RequestType.GetPosts :
@@ -98,9 +111,12 @@ using System.Net.Sockets;
                         {
                             posts = new List<Post>();
                         }
-                        byte[] dataToClientGetPosts =
-                            Encoding.ASCII.GetBytes(JsonSerializer.Serialize(new GetPostsResponse(posts)));
-                        stream.Write(dataToClientGetPosts,0,dataToClientGetPosts.Length);
+                        String getPostsResponseMessage = JsonSerializer.Serialize(new GetPostsResponse(posts));
+                        int toGetPosts = Encoding.ASCII.GetByteCount(getPostsResponseMessage);
+                        byte[] toSendLenGetPosts = BitConverter.GetBytes(toGetPosts);
+                        byte[] dataToClientGetPosts = Encoding.ASCII.GetBytes(getPostsResponseMessage);
+                        client.Send(toSendLenGetPosts);
+                        client.Send(dataToClientGetPosts);
                         break;
                     
                     case RequestType.GetUsers:
@@ -109,34 +125,42 @@ using System.Net.Sockets;
                         {
                             users = new List<User>();
                         }
-                        Console.WriteLine(JsonSerializer.Serialize(new GetUsersResponse(users)));
-                        byte[] dataToClientGetUsers =
-                            Encoding.ASCII.GetBytes(JsonSerializer.Serialize(new GetUsersResponse(users)));
-                        stream.Write(dataToClientGetUsers,0,dataToClientGetUsers.Length);
+                        String getUsersResponseMessage = JsonSerializer.Serialize(new GetUsersResponse(users));
+                        int toGetUsers = Encoding.ASCII.GetByteCount(getUsersResponseMessage);
+                        byte[] toSendLenGetUsers = BitConverter.GetBytes(toGetUsers);
+                        byte[] dataToClientGetUsers = Encoding.ASCII.GetBytes(getUsersResponseMessage);
+                        client.Send(toSendLenGetUsers);
+                        client.Send(dataToClientGetUsers);
                         break;
                     
                     case RequestType.PostUser:
-                        PostUserRequest postUserRequest =
-                            JsonSerializer.Deserialize<PostUserRequest>(
-                                Encoding.ASCII.GetString(dataFromClient, 0, bytesRead));
+                        PostUserRequest postUserRequest = JsonSerializer.Deserialize<PostUserRequest>(message);
                         DbPersistence.AddUser(postUserRequest.User);
-                        stream.Write(dataFromClient,0,dataFromClient.Length);
+                        int toSendPostUser = Encoding.ASCII.GetByteCount(message);
+                        byte[] toSendBytesPostUser = Encoding.ASCII.GetBytes(message);
+                        byte[] toSendLenBytesPostUser = BitConverter.GetBytes(toSendPostUser);
+                        client.Send(toSendLenBytesPostUser);
+                        client.Send(toSendBytesPostUser);
                         break;
                     
                     case RequestType.UpdatePost:
-                        UpdatePostRequest updatePostRequest =
-                            JsonSerializer.Deserialize<UpdatePostRequest>(
-                                Encoding.ASCII.GetString(dataFromClient, 0, dataFromClient.Length));
+                        UpdatePostRequest updatePostRequest = JsonSerializer.Deserialize<UpdatePostRequest>(message);
                         DbPersistence.UpdatePost(updatePostRequest.Post);
-                        stream.Write(dataFromClient,0,dataFromClient.Length);
+                        int toSendUpdatePost = Encoding.ASCII.GetByteCount(message);
+                        byte[] toSendBytesUpdatePost = Encoding.ASCII.GetBytes(message);
+                        byte[] toSendLenBytesUpdatePost = BitConverter.GetBytes(toSendUpdatePost);
+                        client.Send(toSendLenBytesUpdatePost);
+                        client.Send(toSendBytesUpdatePost);
                         break;
                     
                     case RequestType.UpdateUser:
-                        UpdateUserRequest updateUserRequest =
-                            JsonSerializer.Deserialize<UpdateUserRequest>(
-                                Encoding.ASCII.GetString(dataFromClient, 0, dataFromClient.Length));
+                        UpdateUserRequest updateUserRequest = JsonSerializer.Deserialize<UpdateUserRequest>(message);
                         DbPersistence.UpdateUser(updateUserRequest.User);
-                        stream.Write(dataFromClient,0,dataFromClient.Length);
+                        int toSendUpdateUser = Encoding.ASCII.GetByteCount(message);
+                        byte[] toSendBytesUpdateUser = Encoding.ASCII.GetBytes(message);
+                        byte[] toSendLenBytesUpdateUser = BitConverter.GetBytes(toSendUpdateUser);
+                        client.Send(toSendLenBytesUpdateUser);
+                        client.Send(toSendBytesUpdateUser);
                         break;
                 }
                 
